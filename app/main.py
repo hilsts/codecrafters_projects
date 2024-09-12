075a5b77d5cae030a05f236d848fac08c365a86d
hilsts <hilstah@gmail.com>
2024-09-11 22:27:24 -0300

    read blob object

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 import sys
 import os
 import zlib
+import glob
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
     
     command = sys.argv[1]
-    print(command)
+
     if command == "init":
         os.mkdir(".git")
         os.mkdir(".git/objects")
@@ -22,13 +23,13 @@ def main():
         param = sys.argv[2]
         blob_sha = sys.argv[3] 
         if param == "-p":
-            blob_dir = blob_sha[:2]
-            print(blob_sha[2:])
-            content = open(f"./git/{blob_sha[:2]}/{blob_sha[2:]}", "rb").read()
+            content = open(f".git/objects/{blob_sha[:2]}/{blob_sha[2:]}", "rb").read()
+            # content = open(f"./git/objects/{blob_sha}", "rb").read()
             d_content = zlib.decompress(content)
-            print(d_content)
-
-            return d_content
+            str_content = d_content.decode("utf-8")
+            str_content = str_content.split('\x00')
+            str_content = str_content[1]
+            print(str_content, end='')
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
