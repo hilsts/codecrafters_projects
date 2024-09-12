5afd4d9d24c0dd95e5fdf407010c8e145d72856b
hilsts <hilstah@gmail.com>
2024-09-11 23:12:14 -0300

    create blob object

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import zlib
+import hashlib
 import glob
 
 def main():
@@ -30,6 +31,24 @@ def main():
             str_content = str_content.split('\x00')
             str_content = str_content[1]
             print(str_content, end='')
+
+    elif command == "hash-object":
+        param = sys.argv[2]
+        if param == "-w":
+
+            file_path = sys.argv[3]
+
+            content = open(file_path, 'r').read()
+            m = hashlib.sha1((f"blob {len(content.encode('utf-8'))}\0"+content).encode('utf-8'))
+            hash_key = m.hexdigest()
+            print(hash_key, end='')
+            c_content = zlib.compress((f"blob {len(content.encode('utf-8'))}\0"+content).encode('utf-8'))
+            os.mkdir(f".git/objects/{hash_key[:2]}")
+            f = open(f".git/objects/{hash_key[:2]}/{hash_key[2:]}", "wb")
+            f.write(c_content)
+            f.close()
+        
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
